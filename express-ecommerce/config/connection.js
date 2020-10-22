a9e29d04d407d087da17bd9fad41cc08ef05f5e7
Jishnu Satheesh <34735680+jscode99@users.noreply.github.com>
2020-10-22 09:09:19 +0530

    initial commit

---
+++ express-ecommerce/config/connection.js
@@ -0,0 +1,22 @@
+const mongoClient = require('mongodb').MongoClient
+const state = {
+    db:null
+}
+
+module.exports.connect=function (done) {
+    const url = 'mongodb://localhost:27017'                                                                
+    const dbname = 'shopping'
+    
+
+    mongoClient.connect(url, (err, client) => {
+        if (err) return done(err)
+        state.db = client.db(dbname)
+        done();
+        // console.log(state.db);
+    })        
+  
+}
+
+module.exports.get = function () {
+    return state.db
+}
---
+++ routes/admin.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var router = express.Router();
+var productHelper = require("../helpers/product-helpers");
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  productHelper.getAllProducts().then(products => {
+    console.log(products);
+    res.render("admin/veiw-products", {products, admin: true});
+  });
+});
+
+router.get("/add-product", (req, res) => {
+  res.render("admin/add-product");
+});
+
+// admin/add-product/ route's function happens here....
+
+router.post("/add-product", (req, res) => {
+  console.log(req.body);
+  console.log(req.files.Image);
+
+  productHelper.addProduct(req.body, id => {
+    let image = req.files.Image;
+    image.mv("./public/product-images/" + id + ".jpg", (err, done) => {
+      if (!err) {
+        res.render("admin/add-product");
+      } else {
+        console.log(err);
+      }
+    });
+  });
+});
+
+module.exports = router;
